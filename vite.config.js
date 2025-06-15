197e370dec529d53c289a20cbb86f621a6b5670d
vkumala <92326579+vkumala@users.noreply.github.com>
2025-06-15 21:32:24 +0200

    added base to vite.config.js
    aa

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react()],  
+  base: '/space-tourism-website-main-react/', // <-- Set your base path here
+
 })
