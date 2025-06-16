a0e0ec8c31d8199dcd62ed2382603b168bf2bfea
vkumala <92326579+vkumala@users.noreply.github.com>
2025-06-16 19:10:56 +0200

    added base

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],  
-  // base: '/space-tourism-website-main-react/', 
+  base: '/space-tourism-website-main-react/', 
 
 })
